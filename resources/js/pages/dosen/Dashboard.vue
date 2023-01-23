0c6fdb77d4d60d03efc35c2dc3ecf3ac86018515
Prima Giant <kadekprimagiantmartadinata@gmail.com>
2023-01-24 07:32:56 +0800

    ui update

--- resources/js/pages/dosen/Dashboard.vue
+++ resources/js/pages/dosen/Dashboard.vue
@@ -7,36 +7,21 @@
         <!-- Loop Items -->
         <div v-for="kls in kelas" :key="kls">
             <div
-                class="mt-4 h-500-px relative flex flex-col min-w-0 break-words w-full shadow-lg rounded-lg bg-emerald-500"
+                class="mt-4 h-[250px] relative flex flex-col min-w-0 break-words w-full shadow-lg rounded-lg bg-emerald-500"
             >
                 <img
                     alt="..."
-                    src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80"
-                    class="w-full h-[280px] object-cover align-middle rounded-t-lg"
+                    src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=lQT_bOWtysE&auto=format&fit=crop&w=500&q=80"
+                    class="w-full h-[150px] object-cover align-middle rounded-t-lg"
                 />
-                <blockquote class="relative p-8">
-                    <svg
-                        preserveAspectRatio="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 583 95"
-                        class="absolute left-0 w-full block h-95-px -top-94-px"
-                    >
-                        <polygon
-                            points="-30,95 583,95 583,65"
-                            class="text-emerald-500 fill-current"
-                        ></polygon>
-                    </svg>
-                    <h4 class="text-xl font-bold text-white">
-                        {{ kls.nama_mataKuliah }}
+                <blockquote class="relative p-4">
+                    <h4 class="text-xl font-bold text-white uppercase">
+                        {{ kls.nama_kelas }} - [ {{ kls.semester_kelas }}
+                        {{ kls.tahun_kelas }}/{{ kls.tahun_kelas + 1 }} ]
                     </h4>
-                    <p
-                        class="text-md font-light mt-2 text-white block h-[76px] text-ellipsis overflow-hidden"
-                    >
-                        {{ kls.cpmk }}
-                    </p>
                 </blockquote>
-                <div class="px-8 pb-4 absolute bottom-0 right-0">
-                    <router-link :to="`/d/setting/${kls.id_matakuliah}`">
+                <div class="px-4 pb-4 absolute bottom-0 right-0">
+                    <router-link :to="`/d/setting/${kls.id_kelas}`">
                         <button
                             class="bg-indigo-500 text-white active:bg-teal-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                             type="button"
@@ -58,20 +43,21 @@ import { onMounted, ref } from "@vue/runtime-core";
 
 const authStore = useAuthStore();
 const kelas = ref([]);
-const getKelas = async () => {
+
+const getPengampuan = async () => {
     await axios
-        .get("/api/getKelas", {
+        .get("/api/getPengampuanKelas", {
             headers: {
                 Authorization: `Bearer ${authStore.authUser.api_token}`,
             },
         })
         .then((res) => {
-            kelas.value = res.data;
-            console.log(res.data);
+            kelas.value = res.data.kelas;
+            console.log(res.data.kelas[0]);
         });
 };
 
 onMounted(() => {
-    getKelas();
+    getPengampuan();
 });
 </script>
